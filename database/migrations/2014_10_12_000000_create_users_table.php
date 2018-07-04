12a292d21f36ab898b1532cd5ab9fe374c71eaed
Nasrul Hazim <nasrulhazim.m@gmail.com>
2018-07-04 14:45:00 +0800

    Added Ic Number field

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,7 @@ class CreateUsersTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('ic_number', 12)->unique();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps(); // created_at, updated_at
